dc2ab3c62801d72d98be9c2d857f905e7871efc7
Lucemans <luc@lucemans.nl>
2021-10-30 15:27:50 +0200

    Async Testing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export const useYup = () => {
-    
-};
+import { NextFunction, Request, RequestHandler } from "express";
+import { AnySchema, InferType, ValidationError } from "yup";
+
+export type ValidatedRequest<T extends AnySchema> = Request & {
+    data?: InferType<T> | Promise<InferType<T>>
+}
+
+export const useYup = <T extends AnySchema>(schema: T, selectorFn: (req: Request) => { [k: string]: any } = (req) => req.body): RequestHandler => {
+    return (req: ValidatedRequest<T>, res, next: NextFunction) => {
+        req.data = schema.validate(selectorFn(req), { abortEarly: false });
+        next();
+    }
+}
