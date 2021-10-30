a1ed237cca03d34fd7f9e03d4b049e8122971814
Lucemans <luc@lucemans.nl>
2021-10-30 15:27:50 +0200

    Async Testing

--- __tests__/useYup.spec.ts
+++ __tests__/useYup.spec.ts
@@ -1,3 +1,9 @@
+import { useYup } from '../lib';
+import * as yup from 'yup';
+
 it('Performs basic test', () => {
-    expect(true);
+    expect(useYup(yup.object({
+        name: yup.string().length(5),
+        age: yup.number().positive()
+    })));
 });
